309b3e2b3103a429512f4d0fd3f6e352c53fa4b8
AlexisColinMoitry <36001516+AlexisColinMoitry@users.noreply.github.com>
2018-02-05 11:50:14 +0100

    creation du dossier Class
    le dossier sert a ranger tout les Class lier a la BDD

---
+++ Class/Data_Access_Layer/DAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYOP_Model_DAL.Data_Access_Layer
+{
+    class DAL
+    {
+    }
+}
---
+++ Class/Models/Compte.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MYOP_Model_DAL.Models
+{
+    class Compte
+    {
+        //contenue de la table compte
+        private string _id_compte;
+        private string _nom_de_compte;
+        
+        //contenue des info lier au compte dans les autres table
+        private string _id_contenue;
+        private string _nom; // nom du proprietaire
+        private string _prenom;
+        private string _adresse;
+        private string _no_telephone;
+        private string _email;
+        private string _nom_pizzeria;// nom de la propriete
+        private string _adresse_pizzeria;
+
+
+        #region Encapsulation
+        
+        public string Id_compte
+        {
+            get { return _id_compte; }
+            set { _id_compte = value; }
+        }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_-]{1,25}$",
+         ErrorMessage = "Characters are not allowed.")]
+        public string Nom_de_compte
+        {
+            get { return _nom_de_compte; }
+            set { _nom_de_compte = value; }
+        }
+
+        public string Id_contenue
+        {
+            get { return _id_contenue; }
+            set { _id_contenue = value; }
+        }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z -]{1,25}$",
+         ErrorMessage = "Characters are not allowed.")]
+        public string Nom
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z -]{1,25}$",
+         ErrorMessage = "Characters are not allowed.")]
+        public string Prenom
+        {
+            get { return _prenom; }
+            set { _prenom = value; }
+        }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9, -]{1,100}$",
+         ErrorMessage = "Characters are not allowed.")]
+        public string Adresse
+        {
+            get { return _adresse; }
+            set { _adresse = value; }
+        }
+        [Phone]
+        public string No_telephone
+        {
+            get { return _no_telephone; }
+            set { _no_telephone = value; }
+        }
+        [Required]
+        [EmailAddress]
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        [RegularExpression(@"^[a-zA-Z -]{1,25}$",
+         ErrorMessage = "Characters are not allowed.")]
+        public string Nom_pizzeria
+        {
+            get { return _nom_pizzeria; }
+            set { _nom_pizzeria = value; }
+        }
+        [RegularExpression(@"^[a-zA-Z -]{1,25}$",
+         ErrorMessage = "Characters are not allowed.")]
+        public string Adresse_pizzeria
+        {
+            get { return _adresse_pizzeria; }
+            set { _adresse_pizzeria = value; }
+        }
+
+        #endregion
+
+
+        /// <summary>
+        /// constructeur par default non utiliser
+        /// </summary>
+        public Compte() 
+        {
+
+        }
+        /// <param name="list">nom, prenom, adresse, n° de telephone</param>
+        public Compte(params string[] list)
+        {
+            Id_compte = list[0];
+            Nom_de_compte = list[1];
+            Id_contenue = list[2];
+            Nom = list[3];
+            Prenom = list[4];
+            Adresse = list[5];
+            No_telephone = list[6];
+            Email = list[7];
+            if (8 < list.Length) 
+            {
+                Nom_pizzeria = list[8];
+                Adresse_pizzeria = list[9];
+            }
+        }
+
+
+        //private ;
+    }
+
+
+}
