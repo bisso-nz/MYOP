9ee680cd9e1f7ae80e8cc24620d82d79872efd9d
bisso-nz <33216580+bisso-nz@users.noreply.github.com>
2018-02-08 12:00:35 +0100

    Ajout projet myop_ihm
    Site vitrine fait (amélioration nécessaire non responsif)
    page connexion ok

---
+++ myop_ihm/myop_ihm/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myop_ihm.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Presentation()
+        {
+            return View();
+        }
+
+        public ActionResult Clients()
+        {
+            return View();
+        }
+
+        public ActionResult Pricing()
+        {
+            return View();
+        }
+
+        public ActionResult Faq()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Connexion()
+        {
+            return View();
+        }
+
+        public ActionResult Inscription()
+        {
+            return View();
+        }
+
+	}
+}
