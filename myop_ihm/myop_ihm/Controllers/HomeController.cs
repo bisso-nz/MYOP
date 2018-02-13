772f93f4a7543624284450af3f1a9a537fd59f37
bisso-nz <33216580+bisso-nz@users.noreply.github.com>
2018-02-13 11:36:17 +0100

    maj myop_ihm
    layout admin html/bootstrap ok

--- myop_ihm/myop_ihm/Controllers/HomeController.cs
+++ myop_ihm/myop_ihm/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using myop_ihm.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -45,10 +46,69 @@ namespace myop_ihm.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Connexion(string txtNomCompte, string txtMdp)
+        {
+
+            Compte o;
+
+            try
+            {
+
+                    o = DAL.DAL.Connection(txtNomCompte, txtMdp);
+            }
+
+            catch
+            {
+                ViewBag.ErrorConn = "Nom de compte et/ou mot de passe incorrect !";
+                return View();
+            }
+
+            ViewBag.ErrorConn = "";
+            Session["Compte"] = o;
+
+            return Content("OK");
+
+        }
+
         public ActionResult Inscription()
         {
             return View();
         }
 
-	}
+        [HttpPost]
+        public ActionResult Inscription(Compte nouveau, string txtMdp)
+        {
+            nouveau.Type_de_compte = "pizzeria";
+            nouveau.Id_compte = nouveau.Id_contenue = "0";
+
+            try
+            {
+
+                DAL.DAL.CreationStandard(nouveau, txtMdp);
+            }
+            catch
+            {
+                return Content("aïe ! ");
+            }
+
+            Session["Compte"] = nouveau;
+
+            Compte o;
+
+            try
+            {
+
+                o = DAL.DAL.Connection(nouveau.Nom_de_compte, txtMdp);
+            }
+
+            catch
+            {
+                return View();
+            }
+
+            return Content("Bonjour " + nouveau.Nom_de_compte);
+        }
+
+    }
 }
