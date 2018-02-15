949f0b3f96b0e8405fd6d9411c1aadf65a53736e
AlexisColinMoitry <36001516+AlexisColinMoitry@users.noreply.github.com>
2018-02-15 09:04:16 +0100

    upload

---
+++ myop_ihm/Controllers/PizzeriaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myop_ihm.Controllers
+{
+    public class PizzeriaController : Controller
+    {
+        //
+        // GET: /Pizzeria/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
